23c6ead5fa164ca8d71df56ff0bc58cf0236c170
Gurram Mahesh <mahee9825@gmail.com>
2024-04-27 18:03:17 +0530

    V1.0

---
+++ Server.js
@@ -0,0 +1,31 @@
+const express = require("express")
+const mongoose = require("mongoose")
+const cors = require("cors")
+
+
+const dburl = "mongodb://localhost:27017/sdpproject1"
+mongoose.connect(dburl).then(() => {
+    console.log("Connected to DB Successfully")
+}).catch((err) => {
+    console.log(err.message)
+});
+
+
+const app = express() 
+app.use(cors())
+app.use(express.json())  // to parse JSON data
+
+const adminrouter = require("./routes/adminroutes")
+const studentrouter = require("./routes/studentroutes")
+const recruiterrouter = require("./routes/recruiterroutes")
+
+
+app.use("",adminrouter)   // It  contains all the admin routes
+app.use("",studentrouter) 
+app.use("",recruiterrouter)
+
+
+const port=2004
+app.listen(port,()=>{
+    console.log(`Server is running at port ${port}`)
+})
